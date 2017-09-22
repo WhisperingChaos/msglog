717b08dbdbe27395d549fcdac9e1524ba9dd4044
Rich Moyse <rich@moyse.us>
2017-09-22 17:17:52 -0400

    make it compile

--- logrus/msglogrus.go
+++ logrus/msglogrus.go
@@ -1,39 +1,41 @@
 package msglogrus
 
 import (
+	msg "msgloggit"
 	"runtime"
 	"strconv"
 
 	"github.com/sirupsen/logrus"
 )
 
-func NewError(log *logrus.Logger) (errmsg msg.I) {
-	errmsg = new(msglogError)
-	return
+func NewError(log *logrus.Logger) msg.I {
+	emsg := new(msgLogError)
+	emsg.msgLog.setMsgLog(log, 0)
+	return emsg
 }
 
-func newMsgLog(logger *logrus.Logger, depth int) (ml *msgLog) {
-	ml = new(msgLog)
-	ml.msgLog = logger
+type msgLog struct {
+	logger *logrus.Logger
+	depth  int
+}
+
+func (ml msgLog) setMsgLog(logger *logrus.Logger, depth int) {
+	ml.logger = logger
 	ml.depth = depth
-	return
 }
 
-type msglog struct {
-	log   *logrus.Logger
-	depth int
-}
-
-type msglogError struct {
-	msglog
+type msgLogError struct {
+	msgLog
 }
 
 func (ml msgLogError) P(msg string, val ...map[string]interface{}) {
-	_, pkgnm, lineno, _ := runtime.Caller(depth)
-	lentry := NewEntry(ml.msgLog)
+	_, pkgnm, lineno, _ := runtime.Caller(ml.depth + 3)
+	lentry := logrus.NewEntry(ml.logger)
 	lentry = lentry.WithField("file", pkgnm+":"+strconv.Itoa(lineno))
-	for k, v := range val {
-		lentry = lentry.WithFields(k, v)
+	for _, mp := range val {
+		for k, v := range mp {
+			lentry = lentry.WithField(k, v)
+		}
 	}
 	lentry.Error(msg)
 }
